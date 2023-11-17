27252bb4899a2503d7449e179168e01230325b04
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-17 08:40:49 +0100

    Add files via upload
    1er commit

---
+++ info-projet.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# def display_map(m, d):
+#   for ligne in m:
+#     print("".join([d[x] for x in ligne]))
+
+
+# m = [[0, 0, 0, 1, 1], [0, 0, 0, 0, 1], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]
+# d = {0: " ", 1: "#"}
+# display_map(m, d)
+
+def create_perso(départ):
+    l=["x","y"]
+    d["char"]="o"
+    for x in range(len(départ)):
+        d[l(x)]=départ()
