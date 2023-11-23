8869f62b30c7e46b6779354ad021abe2cd511243
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-23 21:42:17 +0100

    Update info-projet.py
    version quasi final sans supplément

--- info-projet.py
+++ info-projet.py
@@ -4,19 +4,18 @@ Spyder Editor
 
 This is a temporary script file.
 """
-def display_map(m, d):
-  for ligne in m:
-    print("".join([d[x] for x in ligne]))
-
-# m = [[0, 0, 0, 1, 1], [0, 0, 0, 0, 1], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]
-# d = {0: " ", 1: "#"}
-# display_map(m, d)
-
-def create_perso(départ):
-    l=["x","y"]
-    d={}
-    d["char"]="o"
-    for x in range(len(départ)):
-        d[l[x]]=départ[x]
-    return d
-#create_perso((0,0))
+from random import randint
+#affichage des 0 sur la map 
+def genereMat(n,m):
+    MAT=[[0 for j in range(m)]for i in range(n)]
+    for i in range(1,len(MAT)):
+        j=randint(0,len(MAT[i])-1)
+        MAT[i][j-1]=1
+    x=0
+    #Apparition d objet
+    i=randint(-1,4)
+    j=randint(-1,9)
+    if MAT[i][j]==0:
+        MAT[i][randint(0,len(MAT[i])-1)]=2
+    if MAT[i][j]==1:
+        MAT[i][j+1]=2
