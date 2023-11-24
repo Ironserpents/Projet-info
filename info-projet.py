d1e1cf137f70f522496ef5785499296f4ab5e1a5
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-24 05:47:46 +0100

    Programme marche pas, mais en bonne voie
    Il faut corrigé les erreurs

--- info-projet.py
+++ info-projet.py
@@ -5,7 +5,6 @@ Spyder Editor
 This is a temporary script file.
 """
 from random import randint
-#affichage des 0 sur la map 
 def genereMat(n,m):
     MAT=[[0 for j in range(m)]for i in range(n)]
     for i in range(1,len(MAT)):
@@ -19,3 +18,29 @@ def genereMat(n,m):
         MAT[i][randint(0,len(MAT[i])-1)]=2
     if MAT[i][j]==1:
         MAT[i][j+1]=2
+    #placement de trou sur la map
+    while x <5:
+        k=randint(-1,4)
+        l=randint(-1,9)
+        if MAT[k][l]==0:
+            MAT[k][randint(0,len(MAT[k])-1)]=3
+            x+=1
+        if MAT[k][l]==1:
+            x+=1
+        if MAT[k][l]==2:
+            x+=1
+    return MAT
+
+def display_map_and_char(l,la,perso):
+    m=genereMat(l,la)
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if i == perso['x'] and j == perso['y']:
+                print(perso['char'],end='')
+            else:
+                print(dico[m[i][j]],end="")
+            print()
+dico={0:' ',1:'#', 2:'B',3:'X'}
+
+print(display_map_and_char(6,6,(1,1)))
+
