472af72fe57152ada1d275279740349f0232de01
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-24 09:22:32 +0100

    Update info-projet.py

--- info-projet.py
+++ info-projet.py
@@ -12,16 +12,16 @@ def genereMat(n,m):
         MAT[i][j-1]=1
     x=0
     #Apparition d objet
-    i=randint(-1,4)
-    j=randint(-1,9)
+    i=randint(0,n-1)
+    j=randint(0,m-1)
     if MAT[i][j]==0:
         MAT[i][randint(0,len(MAT[i])-1)]=2
     if MAT[i][j]==1:
-        MAT[i][j+1]=2
+        MAT[i][j]=2
     #placement de trou sur la map
     while x <5:
-        k=randint(-1,4)
-        l=randint(-1,9)
+        k=randint(0,n-1)
+        l=randint(0,m-1)
         if MAT[k][l]==0:
             MAT[k][randint(0,len(MAT[k])-1)]=3
             x+=1
@@ -31,16 +31,19 @@ def genereMat(n,m):
             x+=1
     return MAT
 
+def create_perso(pos):
+    (x,y)= pos
+    return {"char":"o",'x':x, 'y':y}
+
+
 def display_map_and_char(l,la,perso):
+    p=create_perso(perso)
     m=genereMat(l,la)
     for i in range(len(m)):
         for j in range(len(m[0])):
-            if i == perso['x'] and j == perso['y']:
-                print(perso['char'],end='')
+            if i == p['x'] and j == p['y']:
+                print(p['char'], end='')
             else:
                 print(dico[m[i][j]],end="")
-            print()
-dico={0:' ',1:'#', 2:'B',3:'X'}
-
-print(display_map_and_char(6,6,(1,1)))
-
+        print()
+#dico={0:' ',1:'#', 2:'B',3:'X'}
