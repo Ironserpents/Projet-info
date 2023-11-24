3f8cdacabcf4194bb1a30c780a327a5c95eeb3c0
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-24 10:00:49 +0100

    Update info-projet.py

--- info-projet.py
+++ info-projet.py
@@ -35,10 +35,11 @@ def create_perso(pos):
     (x,y)= pos
     return {"char":"o",'x':x, 'y':y}
 
+p=create_perso(perso)
+m=genereMat(l,la)
 
 def display_map_and_char(l,la,perso):
-    p=create_perso(perso)
-    m=genereMat(l,la)
+
     for i in range(len(m)):
         for j in range(len(m[0])):
             if i == p['x'] and j == p['y']:
@@ -46,4 +47,20 @@ def display_map_and_char(l,la,perso):
             else:
                 print(dico[m[i][j]],end="")
         print()
-#dico={0:' ',1:'#', 2:'B',3:'X'}
+    letter = input("Entrez une lettre (z, q, s, d): ")
+    update_p(letter, p)
+dico={0:' ',1:'#', 2:'B',3:'X'}
+
+def update_p(letter, p):
+  if letter == "z":
+      p["y"] = p["y"] - 1
+  elif letter == "q":
+      p["x"] = p["x"]- 1
+  elif letter == "s":
+      p["y"] = p["y"] + 1
+  elif letter == "d":
+      p["x"] = p["x"]+ 1
+  else:
+      raise ValueError("La lettre {} n'est pas valide.".format(letter))
+
+display_map_and_char(6,6,(1,1))
