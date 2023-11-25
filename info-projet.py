5c96b5760af6ab58a325a006b923d85727345604
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-25 21:39:50 +0100

    Update info-projet.py
    map aléatoire, avec perso déplacement

--- info-projet.py
+++ info-projet.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 from random import randint
 def genereMat(n,m):
     MAT=[[0 for j in range(m)]for i in range(n)]
@@ -35,31 +29,32 @@ def create_perso(pos):
     (x,y)= pos
     return {"char":"o",'x':x, 'y':y}
 
-p=create_perso(perso)
-m=genereMat(l,la)
-
 def display_map_and_char(l,la,perso):
-
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            if i == p['x'] and j == p['y']:
-                print(p['char'], end='')
-            else:
-                print(dico[m[i][j]],end="")
-        print()
-    letter = input("Entrez une lettre (z, q, s, d): ")
-    update_p(letter, p)
+    letter="s"
+    m=genereMat(l,la)
+    p=create_perso(perso)
+    while letter !="r":
+        for i in range(len(m)):
+            for j in range(len(m[0])):
+                if i == p['x'] and j == p['y']:
+                    print(p['char'], end='')
+                else:
+                    print(dico[m[i][j]],end="")
+            print()
+        letter = input("Entrez une lettre (z, q, s, d): ")
+        update_p(letter, p)
+    
 dico={0:' ',1:'#', 2:'B',3:'X'}
 
 def update_p(letter, p):
   if letter == "z":
-      p["y"] = p["y"] - 1
+      p["x"] = p["x"] - 1
   elif letter == "q":
-      p["x"] = p["x"]- 1
+      p["y"] = p["y"]- 1
   elif letter == "s":
-      p["y"] = p["y"] + 1
+      p["x"] = p["x"] + 1
   elif letter == "d":
-      p["x"] = p["x"]+ 1
+      p["y"] = p["y"]+ 1
   else:
       raise ValueError("La lettre {} n'est pas valide.".format(letter))
 
