3d3fafbfc5bebec0284c896db8b4ddf062031503
Ironserpents <122914849+Ironserpents@users.noreply.github.com>
2023-11-27 21:46:43 +0100

    Update info-projet.py

--- info-projet.py
+++ info-projet.py
@@ -24,12 +24,13 @@ def genereMat(n,m):
             x+=1
         if MAT[k][l]==2:
             x+=1
+    MAT[3][3]=4
     return MAT
-
+#tout les déplacements possible du perso
 def voisinage(point) :
     x,y = point
     return {(x+1,y),(x-1,y),(x,y+1),(x,y-1)}
-
+#vérification que la map peut se faire
 def test(m,e,s) :
     if m[s[0]][s[1]]== 1:
         return False
@@ -39,6 +40,7 @@ def test(m,e,s) :
         cc = cc | voisins
         voisins = {p  for c in cc for p in voisinage(c) if valide2(m,p) and p not in cc} 
     return s in cc
+#verifie que le perso sort pas de la map ou va sur un mur ou trou
 def valide2(m,coord):
     x,y = coord
     if x<0  or y<0 or x>=len(m) or y>=len(m[0]) :
@@ -54,31 +56,35 @@ def create_perso(pos):
     return {"char":"o",'x':x, 'y':y}
 
 
-def display_map_and_char(l,la,perso,a):
-    letter,f,="s",False
-    while f==False:
-        m=genereMat(l,la)
-        p=create_perso(perso)
-        f=test(m,perso,(3,3))
-    while letter !="r":
-        for i in range(len(m)):
-            for j in range(len(m[0])):
-                if i == p['x'] and j == p['y']:
-                    print(p['char'], end='')
-                else:
-                    print(dico[m[i][j]],end="")
-            print()
-        letter = input("Entrez une lettre (z, q, s, d): ")
-        update_p(letter, p,m)
-        if m[p['x']][p['y']] ==3:
-            print("Vous êtes tombé dans un trou")
-            break
-        if p['x']== 3 and p['y'] ==3:
-            print ("Vous avez réussis le niveau",a)
-            a=a+1
-            display_map_and_char(l,la,perso,a)
+def display_map_char_and_objects(l,la,perso):
+    a=1
+    while a!=4:
+        letter,f,="s",False
+        while f==False:
+            m=genereMat(l,la)
+            p=create_perso(perso)
+            f=test(m,perso,(3,3))
+        while letter !="r":
+            for i in range(len(m)):
+                for j in range(len(m[0])):
+                    if i == p['x'] and j == p['y']:
+                        print(p['char'], end='')
+                    else:
+                        print(dico[m[i][j]],end="")
+                print()
+            letter = input("Entrez une lettre (z, q, s, d): ")
+            update_p(letter, p,m)
+            if m[p['x']][p['y']] ==2:
+                m[p['x']][p['y']]=0
+            if m[p['x']][p['y']] ==3:
+                print("Vous êtes tombé dans un trou")
+                break
+            if p['x']== 3 and p['y'] ==3:
+                print ("Vous avez réussis le niveau",a)
+                a=a+1
+                break
             
-dico={0:' ',1:'#', 2:'B',3:'X'}
+dico={0:' ',1:'#', 2:'B',3:'X',4:'E'}
 
 def valide(m,coord):
     x,y = coord['x'], coord['y']
@@ -105,9 +111,6 @@ def update_p(letter, p,m):
         p["y"] = p["y"]+ 1
         if not valide(m,p):
             p["y"] = p["y"]- 1
-    elif letter =="r":
-        print("Vous avez arrêté le jeu.")
     else:
         print("La lettre {} n'est pas valide.".format(letter))
-a=1
-display_map_and_char(6,6,(1,1),a)
+display_map_char_and_objects(6,6,(1,1))
